b2368fee8efe8df1ffd3459200f485f6fe973a07
zhanghaojie <1455123087@qq.com>
2024-07-08 04:37:59 +0800

    feat: Refactor genLocales function to accept input and output paths

--- src/gen-locales.ts
+++ src/gen-locales.ts
@@ -4,22 +4,24 @@ import path from "path";
 import { makeColumnIndexLocaleMap } from "./makeColumnIndexLocaleMap";
 import { makeI18nMessagesMap } from "./makeI18nMessagesMap";
 
-const filePath = path.join(process.cwd(), "i18n-messages.xlsx");
+export function genLocales(inputPath: string, outputPath: string) {
+  const worksheets = xlsx.parse(path.resolve(inputPath));
+  const worksheet = worksheets[0];
+  const [header, ...data] = worksheet.data;
 
-const worksheets = xlsx.parse(filePath);
-const worksheet = worksheets[0];
-const [header, ...data] = worksheet.data;
+  const columnIndexLocaleMap = makeColumnIndexLocaleMap(header);
+  const i18nMessagesMap = makeI18nMessagesMap(data, columnIndexLocaleMap);
 
-const columnIndexLocaleMap = makeColumnIndexLocaleMap(header);
-const i18nMessagesMap = makeI18nMessagesMap(data, columnIndexLocaleMap);
+  fs.mkdirSync(path.resolve(outputPath), { recursive: true });
 
-fs.mkdirSync(path.join(process.cwd(), "locales"), { recursive: true });
+  Object.keys(i18nMessagesMap).forEach((locale) => {
+    const localeFilePath = path.resolve(outputPath, `${locale}.json`);
+    console.log(`Writing to ${localeFilePath}`);
+    fs.writeFileSync(
+      localeFilePath,
+      JSON.stringify(i18nMessagesMap[locale], null, 2)
+    );
+  });
+}
 
-Object.keys(i18nMessagesMap).forEach((locale) => {
-  const localeFilePath = path.join(process.cwd(), `locales/${locale}.json`);
-  console.log(`Writing to ${localeFilePath}`);
-  fs.writeFileSync(
-    localeFilePath,
-    JSON.stringify(i18nMessagesMap[locale], null, 2)
-  );
-});
+genLocales("./i18n-messages.xlsx", "./locales");
