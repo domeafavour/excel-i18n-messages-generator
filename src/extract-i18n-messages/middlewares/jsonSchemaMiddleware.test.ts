0844ce1768c34f17585ae0c19cf8fd35ffa92194
zhanghaojie <1455123087@qq.com>
2025-03-13 00:01:20 +0800

    feat(extract-i18n-messages/middlewares): Add jsonSchemaMiddleware to transform JSON schema properties into translation keys

---
+++ src/extract-i18n-messages/middlewares/jsonSchemaMiddleware.test.ts
@@ -0,0 +1,205 @@
+import { describe, expect, it } from "vitest";
+import { applyMiddlewares } from "../applyMiddlewares";
+import { jsonSchemaMiddleware } from "./jsonSchemaMiddleware";
+
+describe("jsonSchemaMiddleware.test", () => {
+  it("should word with schema declarations", () => {
+    const code = `function Component() {
+      const schema = {
+        type: "object",
+        properties: {
+          name: {
+            title: 'Name',
+            type: "string",
+            placeholder: 'Please enter',
+            description: 'this is your name',
+          },
+          dateRange: {
+            title: 'Date Range',
+            type: "array",
+            placeholder: ['start date', 'end date'],
+            description: 'this is your date range',
+          }
+        },
+      };
+      const schema2 = {
+        type: "object",
+        properties: {
+          age: {
+            title: 'Age',
+            type: "number",
+            widget: 'NumberInput',
+            placeholder: 'Please enter',
+          },
+          fullName: {
+            title: 'Full Name',
+            type: 'object',
+            properties: {
+              firstName: {
+                title: 'First Name',
+                type: 'string',
+                placeholder: 'Please enter',
+              },
+              lastName: {
+                title: 'Last Name',
+                type: 'string',
+                placeholder: 'Please enter',
+              },
+            },
+          },
+        },
+      };
+      return null;
+    }`;
+
+    const expected = `function Component() {
+      const schema = {
+        type: "object",
+        properties: {
+          name: {
+            title: t("Component.schema.properties.name.title"),
+            type: "string",
+            placeholder: t("Component.schema.properties.name.placeholder"),
+            description: t("Component.schema.properties.name.description"),
+          },
+          dateRange: {
+            title: t("Component.schema.properties.dateRange.title"),
+            type: "array",
+            placeholder: [t("Component.schema.properties.dateRange.placeholder.0"), t("Component.schema.properties.dateRange.placeholder.1")],
+            description: t("Component.schema.properties.dateRange.description"),
+          }
+        },
+      };
+      const schema2 = {
+        type: "object",
+        properties: {
+          age: {
+            title: t("Component.schema2.properties.age.title"),
+            type: "number",
+            widget: "NumberInput",
+            placeholder: t("Component.schema2.properties.age.placeholder"),
+          },
+          fullName: {
+            title: t("Component.schema2.properties.fullName.title"),
+            type: "object",
+            properties: {
+              firstName: {
+                title: t("Component.schema2.properties.fullName.properties.firstName.title"),
+                type: "string",
+                placeholder: t("Component.schema2.properties.fullName.properties.firstName.placeholder"),
+              },
+              lastName: {
+                title: t("Component.schema2.properties.fullName.properties.lastName.title"),
+                type: "string",
+                placeholder: t("Component.schema2.properties.fullName.properties.lastName.placeholder"),
+              },
+            },
+          },
+        },
+      };
+      return null;
+    }`;
+    const [actual] = applyMiddlewares(code, [jsonSchemaMiddleware]);
+    expect(actual).toBe(expected);
+  });
+
+  it("should work with jsx attribute schema", () => {
+    const code = `function Component() {
+      return <FormRender schema={{
+        type: 'object',
+        properties: {
+          name: {
+            title: 'Name',
+            type: 'string',
+            placeholder: 'Please enter',
+            description: 'this is your name',
+          },
+          dateRange: {
+            title: 'Date Range',
+            type: 'array',
+            placeholder: ['start date', 'end date'],
+            description: 'this is your date range',
+          }
+        },
+      }} />;
+    }`;
+
+    const expected = `function Component() {
+      return (
+        <FormRender schema={{
+          type: "object",
+          properties: {
+            name: {
+              title: t("Component.FormRender.schema.properties.name.title"),
+              type: "string",
+              placeholder: t("Component.FormRender.schema.properties.name.placeholder"),
+              description: t("Component.FormRender.schema.properties.name.description"),
+            },
+            dateRange: {
+              title: t("Component.FormRender.schema.properties.dateRange.title"),
+              type: "array",
+              placeholder: [t("Component.FormRender.schema.properties.dateRange.placeholder.0"), t("Component.FormRender.schema.properties.dateRange.placeholder.1")],
+              description: t("Component.FormRender.schema.properties.dateRange.description"),
+            }
+          },
+        }} />
+      );
+    }`;
+
+    const [actual] = applyMiddlewares(code, [jsonSchemaMiddleware]);
+    expect(actual).toBe(expected);
+  });
+
+  it("should work with `defineProperties`", () => {
+    const code = `function Component() {
+      return <><PageContainer><h1>title</h1><div className="container"><FormRender schema={{
+        type: 'object',
+        properties: defineProperties({
+          name: {
+            title: 'Name',
+            type: 'string',
+            placeholder: 'Please enter',
+            description: 'this is your name',
+          },
+          dateRange: {
+            title: 'Date Range',
+            type: 'array',
+            placeholder: ['start date', 'end date'],
+            description: 'this is your date range',
+          }
+        }),
+      }} /></div></PageContainer></>;
+    }`;
+    const expected = `function Component() {
+      return (
+        <><PageContainer><h1>title</h1><div className="container"><FormRender schema={{
+          type: "object",
+          properties: defineProperties({
+            name: {
+              title: t("Component.PageContainer.FormRender.schema.properties.name.title"),
+              type: "string",
+              placeholder: t("Component.PageContainer.FormRender.schema.properties.name.placeholder"),
+              description: t("Component.PageContainer.FormRender.schema.properties.name.description"),
+            },
+            dateRange: {
+              title: t("Component.PageContainer.FormRender.schema.properties.dateRange.title"),
+              type: "array",
+              placeholder: [t(
+                "Component.PageContainer.FormRender.schema.properties.dateRange.placeholder.0"
+              ), t(
+                "Component.PageContainer.FormRender.schema.properties.dateRange.placeholder.1"
+              )],
+              description: t(
+                "Component.PageContainer.FormRender.schema.properties.dateRange.description"
+              ),
+            }
+          }),
+        }} /></div></PageContainer></>
+      );
+    }`;
+
+    const [actual, values] = applyMiddlewares(code, [jsonSchemaMiddleware]);
+    expect(actual).toBe(expected);
+    console.log(values)
+  });
+});
---
+++ src/extract-i18n-messages/middlewares/jsonSchemaMiddleware.ts
@@ -0,0 +1,70 @@
+import { defineMiddleware } from "../defineMiddleware";
+import { genMessageKey } from "../utils/genMessageKey";
+import { getNodePaths } from "../utils/getNodePaths";
+import { isCallExpressionNodePath } from "../utils/isCallExpressionNodePath";
+import { isHtmlTagNodePath } from "../utils/isHtmlTagNodePath";
+
+export const jsonSchemaMiddleware = defineMiddleware(
+  (source, messages, { j }) => {
+    const values = { ...messages };
+    const root = j(source);
+
+    // find function declarations with pascal case names
+    root
+      .find(j.FunctionDeclaration, {
+        id: {
+          name: (value) => /^[A-Z]/.test(value),
+        },
+      })
+      .forEach((path) => {
+        // find json schema object
+        j(path)
+          .find(j.ObjectExpression, {
+            properties: [
+              {
+                key: {
+                  name: "type",
+                },
+                value: {
+                  value: "object",
+                },
+              },
+              {
+                key: {
+                  name: "properties",
+                },
+              },
+            ],
+          })
+          .forEach((schemaPath) => {
+            j(schemaPath)
+              .find(j.Literal)
+              .replaceWith((literalPath) => {
+                const { value } = literalPath.node;
+                if (typeof value === "string") {
+                  const messageKey = genMessageKey(
+                    getNodePaths(literalPath).filter(
+                      (p) =>
+                        !isHtmlTagNodePath(p) && !isCallExpressionNodePath(p)
+                    )
+                  );
+                  const shouldReplace =
+                    /(\.title|\.description|\.placeholder(\.\d)?)$/.test(
+                      messageKey
+                    );
+                  if (shouldReplace) {
+                    values[messageKey] = value;
+                    return j.callExpression(j.identifier("t"), [
+                      j.stringLiteral(messageKey),
+                    ]);
+                  }
+                  return j.stringLiteral(value);
+                }
+                return literalPath;
+              });
+          });
+      });
+
+    return [root.toSource(), values];
+  }
+);
