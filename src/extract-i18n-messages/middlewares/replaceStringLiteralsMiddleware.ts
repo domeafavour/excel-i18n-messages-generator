c9631f36b8af5d7aa36d0cf17dd463adcd606cf2
zhanghaojie <1455123087@qq.com>
2025-03-13 00:01:08 +0800

    feat(extract-i18n-messages/replaceStringLiteralsMiddleware): Integrate genMessageKey and filter out HTML tag nodes

--- src/extract-i18n-messages/middlewares/replaceStringLiteralsMiddleware.ts
+++ src/extract-i18n-messages/middlewares/replaceStringLiteralsMiddleware.ts
@@ -1,7 +1,9 @@
 import { defineMiddleware } from "../defineMiddleware";
 import { I18nMessages } from "../typings";
+import { genMessageKey } from "../utils/genMessageKey";
 import { getNodePaths } from "../utils/getNodePaths";
 import { isComponentFunction } from "../utils/isComponentFunction";
+import { isHtmlTagNodePath } from "../utils/isHtmlTagNodePath";
 
 export const replaceStringLiteralsMiddleware = defineMiddleware(
   (source, previousValues, { j }) => {
@@ -12,11 +14,11 @@ export const replaceStringLiteralsMiddleware = defineMiddleware(
       const { value } = path.node;
 
       if (typeof value === "string") {
-        const paths = getNodePaths(path);
+        const paths = getNodePaths(path).filter((p) => !isHtmlTagNodePath(p));
 
         // string variable declaration in a component
         if (paths.length === 2 && paths[0] && isComponentFunction(paths[0])) {
-          const key = paths.map((n) => n.name).join(".");
+          const key = genMessageKey(paths);
           values[key] = value;
           return j.callExpression(j.identifier("t"), [j.stringLiteral(key)]);
         }
